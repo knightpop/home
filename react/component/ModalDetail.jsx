a1d7a28e19c02e6b9f0ba1d26d3854fba5131cbf
ktz <knightpop@gmail.com>
2017-07-01 04:28:10 +0900

    end of 1st commit

---
+++ react/component/ModalDetail.jsx
@@ -0,0 +1,16 @@
+/**
+ * Created by ktz on 2017. 7. 1..
+ */
+
+import React, {Component} from 'react'
+
+export default class ModalDetail extends Component {
+    render() {
+        console.log('Hello World!!!!');
+        return (
+            <div>
+                Hello World!
+            </div>
+        );
+    }
+}
